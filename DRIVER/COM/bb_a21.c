665bcd231da08599d9c3cb15048289448557ff44
KonradJPE <ktomasik@jpembedded.eu>
2019-11-26 12:08:08 +0100

    Remove '(c)' from copyright line

--- DRIVER/COM/bb_a21.c
+++ DRIVER/COM/bb_a21.c
@@ -25,7 +25,7 @@
  *     Switches: _ONE_NAMESPACE_PER_DRIVER_
  *
  *---------------------------------------------------------------------------
- * Copyright (c) 2001-2019, MEN Mikro Elektronik GmbH
+ * Copyright 2001-2019, MEN Mikro Elektronik GmbH
  ******************************************************************************/
 /*
  * This program is free software: you can redistribute it and/or modify
